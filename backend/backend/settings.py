26a57797fa5589073671d7f6a0311def94aed05f
jedi <git@m.j3d1.de>
2023-05-28 15:31:09 +0200

    stash

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -73,14 +73,7 @@ MIDDLEWARE = [
 
 ALLOWED_HOSTS = ["10.23.42.128", "127.0.0.1"]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://10.23.42.128:8000",
-    "http://10.23.42.128:5173",
-    "http://127.0.0.1:8000",
-    "http://127.0.0.1:5173",
-    "http://localhost:8000",
-    "http://localhost:5173",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'backend.urls'
 
